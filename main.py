455790eef6c8f8da00c4b3df831dc35c6d4f473b
mbrsagor <mbrsagor@gmail.com>
2022-11-20 14:50:17 +0600

    New: Fake API done.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 
@@ -24,3 +24,29 @@ class Movie(BaseModel):
 @app.get('/')
 async def root():
     return fake_movie_db
+
+
+@app.post('/create-move/', status_code=201)
+async def add_movie(payload: Movie):
+    movie = payload.dict()
+    fake_movie_db.append(movie)
+    return {'id': len(fake_movie_db) - 1}
+
+
+@app.put('/update-move/{pk}/')
+async def update_move(id: int, payload: Movie):
+    movie = payload.dict()
+    movies_length = len(fake_movie_db)
+    if 0 <= id <= movies_length:
+        fake_movie_db[id] = movie
+        return None
+    raise HTTPException(status_code=404, detail="Movie with given id not found")
+
+
+@app.delete('delete-move/{id}/')
+async def delete_movie(id: int):
+    movies_length = len(fake_movie_db)
+    if 0 <= id <= movies_length:
+        del fake_movie_db[id]
+        return None
+    raise HTTPException(status_code=404, detail="Movie with given id not found")
