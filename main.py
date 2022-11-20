9b74e0e0e529c41bf4a0b3158bf1323840861eb1
mbrsagor <mbrsagor@gmail.com>
2022-11-20 12:18:21 +0600

    New: Use fack API response and make db.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+fake_movie_db = [
+    {
+        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
+        'plot': 'The surviving members of the resistance face the First Order once again.',
+        'genres': ['Action', 'Adventure', 'Fantasy'],
+        'casts': ['Daisy Ridley', 'Adam Driver']
+    }
+]
 
-@app.get("/")
+
+class Movie(BaseModel):
+    name: str
+    plot: str
+    genres: List[str]
+    casts: List[str]
+
+
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return fake_movie_db
